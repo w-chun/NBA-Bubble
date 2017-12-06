2cdbcacb87f5e938d225d5fb6e026590e7fb6e28
Wilson <wchun108@gmail.com>
2017-12-05 22:06:09 -0800

    Can split/combine bubbles

--- bundle.js
+++ bundle.js
@@ -9441,7 +9441,7 @@ var d3 = _interopRequireWildcard(_d);
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 (function () {
-  var width = 500,
+  var width = 1000,
       height = 500;
 
   var svg = d3.select("#chart").append("svg").attr("height", height).attr("width", width).append("g").attr("transform", "translate(0,0)");
@@ -9450,11 +9450,27 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
   defs.append("pattern").attr("id", "jordan-clarkson").attr("height", "100%").attr("width", "100%").attr("patternContentUnits", "objectBoundingBox").append("image").attr("height", 1).attr("weight", 1).attr("preserveAspectRadio", "none").attr("xmlns:xlink", "http://w3.org/1999/xlink").attr("xlink:href", "../images/jc6.png");
 
-  var radiusScale = d3.scaleSqrt().domain([6, 403]).range([10, 50]);
+  var radiusScale = d3.scaleSqrt().domain([6, 403]).range([10, 60]);
 
-  var simulation = d3.forceSimulation().force("x", d3.forceX(width / 2).strength(0.05)).force("y", d3.forceY(height / 2).strength(0.05)).force("collide", d3.forceCollide(function (d) {
+  var forceXSeparate = d3.forceX(function (d) {
+    if (d.fldGoalPct < .5) {
+      return 200;
+    } else {
+      return 750;
+    }
+  }).strength(0.05);
+
+  var forceXCombine = d3.forceX(width / 2).strength(0.05);
+
+  var forceY = d3.forceY(function (d) {
+    return height / 2;
+  }).strength(0.05);
+
+  var forceCollide = d3.forceCollide(function (d) {
     return radiusScale(d.shotsMade) + 1;
-  }));
+  });
+
+  var simulation = d3.forceSimulation().force("x", forceXCombine).force("y", forceY).force("collide", forceCollide);
 
   d3.queue().defer(d3.csv, "../data/lakersFG_2016_2017.csv").await(ready);
 
@@ -9476,6 +9492,14 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
       return d.player.replace(/ /g, "-");
     }).attr("height", "100%").attr("width", "100%").attr("patternContentUnits", "objectBoundingBox").append("image").attr("height", 1).attr("weight", 1).attr("preserveAspectRadio", "none").attr("xmlns:xlink", "http://w3.org/1999/xlink").attr("xlink:href", function (d) {
       return d.imagePath;
+    });
+
+    d3.select("#percent").on('click', function () {
+      simulation.force("x", forceXSeparate).alphaTarget(0.5).restart();
+    });
+
+    d3.select("#combine").on('click', function () {
+      simulation.force("x", forceXCombine).alphaTarget(0.5).restart();
     });
 
     simulation.nodes(datapoints).on('tick', ticked);
